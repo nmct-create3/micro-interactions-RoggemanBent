773bcac0f7ee5b62f51cc41fbf31e7e51bb1997b
RoggemanBent <55881742+RoggemanBent@users.noreply.github.com>
2020-10-06 12:26:56 +0200

    les gedaan

--- script/app.js
+++ script/app.js
@@ -1,13 +1,42 @@
+let email = {},
+    password = {},
+    signInButton;
+
 function handleFloatingLabel() {
    
 }
 
-function handlePasswordSwitcher() {
+// function
+// field: null;
+// email.field = document.qs('.js-email-field')
+
+const handlePasswordSwitcher = function() {
+    const passwordInput = document.querySelector('.js-password-input');
+    const passwordToggle = document.querySelector('.js-password-toggle');
     
+    // simpele zelfcontrole
+    if (!passwordInput || !passwordToggle) {
+        console.error("De classes werden niet gevonden");
+        return;
+    }
+
+    // text tonen of password tonen
+    passwordToggle.addEventListener('change', function() {
+        if (passwordInput.type == 'password') {
+            passwordInput.type = 'text';
+        }
+        else {
+            passwordInput.type = 'password';
+        }
+    });
+}
+
+const getDOMElements = function() {
+
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    console.log('Script loaded!');
-    handleFloatingLabel();
-    handlePasswordSwitcher();
+    console.log('DOM loaded');
+    // handleFloatingLabel();
+    // handlePasswordSwitcher();
 });
